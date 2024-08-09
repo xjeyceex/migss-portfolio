92554dcf1581e0ae8ddf7a7ec0d59bb4c2b3f004
Jc Miguel <157223954+xjeyceex@users.noreply.github.com>
2024-08-09 16:38:57 +0800

    Update ProjectData.js

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -68,6 +68,11 @@ export const stackList = [
   },
 
   {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+    name: "Phyton",
+  },
+
+  {
     img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
     name: "Tailwind CSS",
   },
