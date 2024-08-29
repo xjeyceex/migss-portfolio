71914516dc996213773c208ba25f84ea256cb38e
Jc Miguel <jcmiguel.beltran@gmail.com>
2024-08-30 04:47:07 +0800

    add vercel insights

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import Home from "./pages/Home";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 function App() {
   return (
+    <>
       <Home />
+      <SpeedInsights />
+    </>
   );
 }
 
