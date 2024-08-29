af08da1976ebf04920fde5af1797970120fe496e
Jc Miguel <jcmiguel.beltran@gmail.com>
2024-08-30 06:32:48 +0800

    fix bugs and update version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Home from "./pages/Home";
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 function App() {
   return (
     <>
       <Home />
-      <SpeedInsights />
     </>
   );
 }
