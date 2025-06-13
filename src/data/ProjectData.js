0a4449da29686be86383b2eda1ccad409cdd92e1
xjeyceex <jcmiguel.beltran@gmail.com>
2025-06-13 23:22:58 +0800

    changes

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -3,31 +3,83 @@ export const ProjectList = [
   {
     img: "/canvassing.jpg",
     title: "Canvassly",
-    description: "Canvassly simplifies procurement by streamlining canvassing, supplier comparison, and purchase approvals. Built with Next.js, Supabase, and Mantine, it ensures an efficient and transparent sourcing process.",
-    tech_stack: ["NextJS", "NodeJS", "TypeScript", "Supabase", "PostgreSQL", "Mantine", "Zustand"],
+    description:
+      "Canvassly simplifies procurement by streamlining canvassing, supplier comparison, and purchase approvals. Built with Next.js, Supabase, and Mantine, it ensures an efficient and transparent sourcing process.",
+    tech_stack: [
+      "NextJS",
+      "NodeJS",
+      "TypeScript",
+      "Supabase",
+      "PostgreSQL",
+      "Mantine",
+      "Zustand",
+    ],
     github_url: "https://github.com/xjeyceex/canvassing-app-dev",
     demo_url: "https://canvassly.vercel.app",
   },
   {
     img: "/help-earth.jpg",
     title: "Help You",
-    description: "a responsive web app delivering climate change data by U.S. state and county. Handled front-end design, back-end optimization, and end-to-end bug fixing for a smooth user experience.",
-    tech_stack: ["NextJS", "NodeJS", "TypeScript", "NoSQL", "Prisma", "Tailwind CSS"],
+    description:
+      "a responsive web app delivering climate change data by U.S. state and county. Handled front-end design, back-end optimization, and end-to-end bug fixing for a smooth user experience.",
+    tech_stack: [
+      "NextJS",
+      "NodeJS",
+      "TypeScript",
+      "NoSQL",
+      "Prisma",
+      "Tailwind CSS",
+    ],
     github_url: "https://github.com/xjeyceex/help-earth-now",
     demo_url: "https://help-earth-now.vercel.app",
   },
   {
+    img: "/pesowise.png",
+    title: "PesoWise",
+    description:
+      "Pesowise is a personal finance progressive web app designed to help individuals make informed spending decisions and track their money with clarity. It includes modules like a Worth It Calculator, Money Tracker, Budget Dashboard, and Decision Assistant. I built this project solo, using full-stack technologies and UI libraries to deliver a seamless user experience across devices.",
+    tech_stack: [
+      "Next.js",
+      "React",
+      "Tailwind CSS",
+      "Shadcn/ui",
+      "TypeScript",
+      "Vercel",
+      "PWA",
+      "LocalStorage",
+    ],
+    github_url: "https://github.com/xjeyceex/financial-app",
+    demo_url: "https://financial-app-mocha.vercel.app",
+  },
+  {
     img: "/image.png",
     title: "Direct Client",
-    description: "In my work with a direct client as a software developer, I specialized in optimizing, enhancing, fixing, and redesigning applications using both front-end and back-end technologies. I also created engaging promotional materials to attract client interest, ensuring an enhanced user experience. I collaborated on projects including a time ticketing app and web HR systems.",
-    tech_stack: ["PostgreSQL", "React.js", "Next.js", "SourceTree", "Git", "Express.js", "Node.js", "HTML", "CSS", "Tailwind CSS", "Material UI", "JavaScript", "TypeScript", "Figma"],
+    description:
+      "In my work with a direct client as a software developer, I specialized in optimizing, enhancing, fixing, and redesigning applications using both front-end and back-end technologies. I also created engaging promotional materials to attract client interest, ensuring an enhanced user experience. I collaborated on projects including a time ticketing app and web HR systems.",
+    tech_stack: [
+      "PostgreSQL",
+      "React.js",
+      "Next.js",
+      "SourceTree",
+      "Git",
+      "Express.js",
+      "Node.js",
+      "HTML",
+      "CSS",
+      "Tailwind CSS",
+      "Material UI",
+      "JavaScript",
+      "TypeScript",
+      "Figma",
+    ],
     github_url: "",
     demo_url: "",
   },
   {
     img: "/gym-app.jpg",
     title: "Evo Gym",
-    description: "A state-of-the-art gym app, powered by TypeScript, Tailwind CSS, and React JS. It offers a seamless user experience, a responsive design that looks great on any device, and a wide range of features to help users stay on track and achieve their fitness goals.",
+    description:
+      "A state-of-the-art gym app, powered by TypeScript, Tailwind CSS, and React JS. It offers a seamless user experience, a responsive design that looks great on any device, and a wide range of features to help users stay on track and achieve their fitness goals.",
     tech_stack: ["TypeScript", "React", "Tailwind"],
     github_url: "https://github.com/xjeyceex/gym-app",
     demo_url: "https://gym-app-azure.vercel.app/",
@@ -35,7 +87,8 @@ export const ProjectList = [
   {
     img: "/cloud.png",
     title: "Decentralized Cloud",
-    description: "Introducing decentralized cloud: Access data securely from any device, maintain complete privacy with AES encryption, and share seamlessly with friends—all. Welcome to the future of cloud storage, where simplicity meets security.",
+    description:
+      "Introducing decentralized cloud: Access data securely from any device, maintain complete privacy with AES encryption, and share seamlessly with friends—all. Welcome to the future of cloud storage, where simplicity meets security.",
     tech_stack: ["React", "Javascript", "HTML", "CSS", "Solidity"],
     github_url: "",
     demo_url: "",
@@ -43,7 +96,8 @@ export const ProjectList = [
   {
     img: "/blog-ninja.jpg",
     title: "Ninja Blog",
-    description: "This blog website showcases the implementation of cutting-edge technologies such as Node.js, Express.js, EJS (Embedded JavaScript), and MongoDB to deliver a dynamic and feature-rich experience",
+    description:
+      "This blog website showcases the implementation of cutting-edge technologies such as Node.js, Express.js, EJS (Embedded JavaScript), and MongoDB to deliver a dynamic and feature-rich experience",
     tech_stack: ["Node.js", "MongoDB", "EJS"],
     github_url: "",
     demo_url: "",
@@ -57,20 +111,20 @@ export const stackList = [
     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
     name: "HTML",
   },
-  
+
   {
     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
     name: "CSS",
   },
-  
+
   {
     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
     name: "JavaScript",
   },
 
   {
-  img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
-  name: "TypeScript",
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    name: "TypeScript",
   },
 
   {
@@ -127,7 +181,7 @@ export const stackList = [
     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg",
     name: "Figma",
   },
-  
+
   {
     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/prisma/prisma-original.svg",
     name: "Prisma",
@@ -137,6 +191,4 @@ export const stackList = [
     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sqlite/sqlite-original.svg",
     name: "SQLite",
   },
-
-
 ];
