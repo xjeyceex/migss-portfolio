bfeeeaaab71007a9197bbaea4a7ed4d4ee544655
Migss <miguel@thedigitalroom.com.au>
2026-01-19 22:08:20 +0800

    update resume

--- src/components/SocialIcon/FixSocialIcon.js
+++ src/components/SocialIcon/FixSocialIcon.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaLinkedin, FaGithub, FaInstagram, FaFacebook } from "react-icons/fa";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 import styled from "@emotion/styled";
 import ScrollAnimation from "react-animate-on-scroll";
 
